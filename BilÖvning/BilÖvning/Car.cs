18f239436d918c55b898957dd0c3558c4ba3b6da
MyNameJaeff <fredrik.andersson@chasacademy.se>
2024-09-30 15:49:30 +0200

    Something?

---
+++ BilÖvning/BilÖvning/Car.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilÖvning
+{
+    internal class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public double Milage { get; set; }
+        public string Color { get; set; }
+        public string FuelType { get; set; }
+
+        public Car(string make, string model, string color,int year, double milage, string fuelType)
+        {
+            Make = make;
+            Model = model;
+            Color = color;
+            Year = year;
+            Milage = milage;
+            FuelType = fuelType;
+        }
+
+        public void Drive(double kilometer)
+        {
+            Milage += kilometer / 10; // Delat med tio för kilometer till mil
+        }
+
+        public void CheckService()
+        {
+            Console.WriteLine((Milage >= 2000) ? "Bilen behöver åka in på service!" : "Bilen behöver inte service än!"); // Ifall över 2000 mil skriv service annars inte
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Make: {Make}, Model: {Model}, Color: {Color},Year: {Year}, Milage: {Milage}, FuelType: {FuelType}"); // Printar all info
+        }
+    }
+}
---
+++ BilÖvning/BilÖvning/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace BilÖvning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("Toyota", "Supra", "White", 1999, 0.0, "Disel");
+            car1.DisplayInfo();
+            car1.Drive(19999);
+            car1.DisplayInfo();
+            car1.CheckService();
+        }
+    }
+}
